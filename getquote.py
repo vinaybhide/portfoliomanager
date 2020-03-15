2501c8fc07420821bb30dd9d36cd1b4cffc6a4c7
vinaybhide <60889328+vinaybhide@users.noreply.github.com>
2020-03-15 20:30:57 +0530

    Adding plot to the dialog

--- getquote.py
+++ getquote.py
@@ -4,10 +4,20 @@ from tkinter import messagebox as msgbx
 from datetime import date
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
+from matplotlib.pyplot import Figure
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib import interactive
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from datetime import date
+
 
 class classGetQuote(Toplevel):
     def __init__(self, master=None, argkey='XXXX', argscript=""):
         Toplevel.__init__(self, master=master)
+
+        self.wm_state(newstate='zoomed')
+        self.wm_resizable(width=False, height=False)
         self.key = argkey
         self.script = argscript
         #self.exchange_text = StringVar()
@@ -20,7 +30,7 @@ class classGetQuote(Toplevel):
             #self.exchange_text.set(argscript.split(':')[0])
             #self.symbol_text.set(argscript.split(':')[1])
             self.search_symbol_combo_text.set(argscript)
-        self.configure(padx=5, pady=10)
+        #self.configure(padx=5, pady=10)
 
         self.iscancel = False
         # Now create exchange label and combo box to show exchange along with associated text variable to hold selection
@@ -35,9 +45,9 @@ class classGetQuote(Toplevel):
         self.search_symbol_combo_text = StringVar()
         self.search_symbol_combo = ttk.Combobox(self, textvariable=
         self.search_symbol_combo_text, 
-            state='normal', width=100, postcommand=self.commandSearchSymbol)
+            state='normal', postcommand=self.commandSearchSymbol)
         
-        self.search_symbol_combo.bind('<Return>', self.commandSearchSymbol)
+        self.search_symbol_combo.bind('<Return>', self.commandEnterKey)
 
         self.open_label = ttk.Label(self, text='Open: ')
         self.open_val_label = ttk.Label(self, text='')
@@ -58,6 +68,12 @@ class classGetQuote(Toplevel):
         self.changepct_label = ttk.Label(self, text='Change %: ')
         self.changepct_val_label = ttk.Label(self, text='')
 
+
+        self.f = Figure(figsize=(12,8), dpi=100, facecolor='w', edgecolor='k', tight_layout=True)
+        self.output_canvas=FigureCanvasTkAgg(self.f, master=self)
+        self.toolbar_frame=Frame(master=self)
+        self.toolbar = NavigationToolbar2Tk(self.output_canvas, self.toolbar_frame)
+
         #if(len(self.script)>0):
         #    if(((argscript.split(':')[0]).find('BSE')) >= 0):
         #        self.exchange_combo.current(0)
@@ -70,35 +86,39 @@ class classGetQuote(Toplevel):
         self.btn_cancel = ttk.Button(self, text="Cancel", command=self.btnCancel)
 
         #put widgets on grid_configure
-        #self.exchange_label.grid_configure(row=0, column=0, sticky=(N, W), padx=5, pady=5)
-        #self.exchange_combo.grid_configure(row=0, column=1, sticky=(N, W), padx=5, pady=5)
-        #self.symbol_label.grid_configure(row=0, column=2, sticky=(N, W), padx=5, pady=5)
-        #self.symbol_entry.grid_configure(row=0, column=3, sticky=(N, W), padx=5, pady=5)
+        #self.exchange_label.grid_configure(row=0, column=0, sticky=(N, W))
+        #self.exchange_combo.grid_configure(row=0, column=1, sticky=(N, W))
+        #self.symbol_label.grid_configure(row=0, column=2, sticky=(N, W))
+        #self.symbol_entry.grid_configure(row=0, column=3, sticky=(N, W))
 
-        self.search_symbol_label.grid_configure(row=0, column=0, sticky=(NE), padx=5, pady=5)
-        self.search_symbol_combo.grid_configure(row=0, column=1, columnspan=5, sticky=(NW), padx=5, pady=5)
+        self.search_symbol_label.grid_configure(row=0, column=0, sticky='NE')
+        self.search_symbol_combo.grid_configure(row=0, column=1, sticky='NSEW', columnspan = 2)
 
-        self.open_label.grid_configure(row=1, column=0, sticky=('NW'), padx=5, pady=5)
-        self.open_val_label.grid_configure(row=1, column=1, sticky=('NE'), padx=5, pady=5)
-        self.high_label.grid_configure(row=1, column=2, sticky=('NW'), padx=5, pady=5)
-        self.high_val_label.grid_configure(row=1, column=3, sticky=('NE'), padx=5, pady=5)
-        self.low_label.grid_configure(row=1, column=4, sticky=('NW'), padx=5, pady=5)
-        self.low_val_label.grid_configure(row=1, column=5, sticky=('NE'), padx=5, pady=5)
-        self.price_label.grid_configure(row=1, column=6, sticky=('NW'), padx=5, pady=5)
-        self.price_val_label.grid_configure(row=1, column=7, sticky=('NE'), padx=5, pady=5)
-        self.volume_label.grid_configure(row=1, column=8, sticky=('NE'), padx=5, pady=5)
-        self.volume_val_label.grid_configure(row=1, column=9, sticky=('NW'), padx=5, pady=5)
-        self.latesttradingday_label.grid_configure(row=2, column=0, sticky=('NE'), padx=5, pady=5)
-        self.latesttradingday_val_label.grid_configure(row=2, column=1, sticky=('NW'), padx=5, pady=5)
-        self.prevclose_label.grid_configure(row=2, column=2, sticky=('NE'), padx=5, pady=5)
-        self.prevclose_val_label.grid_configure(row=2, column=3, sticky=('NW'), padx=5, pady=5)
-        self.change_label.grid_configure(row=2, column=4, sticky=('NE'), padx=5, pady=5)
-        self.change_val_label.grid_configure(row=2, column=5, sticky=('NW'), padx=5, pady=5)
-        self.changepct_label.grid_configure(row=2, column=6, sticky=('NE'), padx=5, pady=5)
-        self.changepct_val_label.grid_configure(row=2, column=7, sticky=('NW'), padx=5, pady=5)
+        self.open_label.grid_configure(row=1, column=0, sticky='NE')
+        self.open_val_label.grid_configure(row=1, column=1, sticky='NW')
+        self.high_label.grid_configure(row=1, column=2, sticky='NE')
+        self.high_val_label.grid_configure(row=1, column=3, sticky='NW')
+        self.low_label.grid_configure(row=1, column=4, sticky='NE')
+        self.low_val_label.grid_configure(row=1, column=5, sticky='NW')
+        self.price_label.grid_configure(row=2, column=0, sticky='NE')
+        self.price_val_label.grid_configure(row=2, column=1, sticky='NW')
+        self.volume_label.grid_configure(row=2, column=2, sticky='NE')
+        self.volume_val_label.grid_configure(row=2, column=3, sticky='NW')
+        self.latesttradingday_label.grid_configure(row=2, column=4, sticky='NE')
+        self.latesttradingday_val_label.grid_configure(row=2, column=5, sticky='NW')
+        self.prevclose_label.grid_configure(row=3, column=0, sticky='NE')
+        self.prevclose_val_label.grid_configure(row=3, column=1, sticky='NW')
+        self.change_label.grid_configure(row=3, column=2, sticky='NE')
+        self.change_val_label.grid_configure(row=3, column=3, sticky='NW')
+        self.changepct_label.grid_configure(row=3, column=4, sticky='NE')
+        self.changepct_val_label.grid_configure(row=3, column=5, sticky='NW')
 
-        self.btn_get_quote.grid_configure(row=3, column=2, padx=5, pady=5)
-        self.btn_cancel.grid_configure(row=3, column=3, padx=5, pady=5)
+        self.btn_get_quote.grid_configure(row=4, column=3)
+        self.btn_cancel.grid_configure(row=4, column=4)
+
+        self.output_canvas.get_tk_widget().grid(row=5, column=0, columnspan=11, sticky=(N, E, W, S))
+        self.toolbar_frame.grid(row=6, column=0, columnspan=11, sticky=(N, E, W, S))
+        self.toolbar.grid(row=0, column=0, sticky=(N, W))
 
         """        self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -124,7 +144,7 @@ class classGetQuote(Toplevel):
 
     def show(self):
         self.wm_deiconify()
-        self.search_symbol_combo.focus_set()
+        self.search_symbol_combo.focus_force()
         self.wait_window()
     """if(self.iscancel == True):
             return None
@@ -154,15 +174,15 @@ class classGetQuote(Toplevel):
             quote_tuple=ts.get_quote_endpoint(symbol=self.script)
             #quote_tuple[0].values[0][1]
             #for i in range(1, 1, quote_tuple[0].size):
-            self.open_label.configure(text="Open:  "+ quote_tuple[0].values[0][1])
-            self.high_label.configure(text="High:  "+ quote_tuple[0].values[0][2])
-            self.low_label.configure(text="Low:  "+ quote_tuple[0].values[0][3])
-            self.price_label.configure(text="Price:  "+ quote_tuple[0].values[0][4])
-            self.volume_label.configure(text="Volume:  "+ quote_tuple[0].values[0][5])
-            self.latesttradingday_label.configure(text="Latest trading day:  "+ quote_tuple[0].values[0][6])
-            self.prevclose_label.configure(text="Previous close:  "+ quote_tuple[0].values[0][7])
-            self.change_label.configure(text="Change:  "+ quote_tuple[0].values[0][8])
-            self.changepct_label.configure(text="Change percent:  "+ quote_tuple[0].values[0][9])
+            self.open_val_label.configure(text=quote_tuple[0].values[0][1])
+            self.high_val_label.configure(text=quote_tuple[0].values[0][2])
+            self.low_val_label.configure(text=quote_tuple[0].values[0][3])
+            self.price_val_label.configure(text=quote_tuple[0].values[0][4])
+            self.volume_val_label.configure(text=quote_tuple[0].values[0][5])
+            self.latesttradingday_val_label.configure(text=quote_tuple[0].values[0][6])
+            self.prevclose_val_label.configure(text=quote_tuple[0].values[0][7])
+            self.change_val_label.configure(text=quote_tuple[0].values[0][8])
+            self.changepct_val_label.configure(text=quote_tuple[0].values[0][9])
         except Exception as e:
             msgbx.showerror("Get Quote Error", str(e))
             self.focus_force()
@@ -183,8 +203,11 @@ class classGetQuote(Toplevel):
                 search_values_list.append(self.searchTuple[0].values[i][0] + "--" + self.searchTuple[0].values[i][1])
 
             self.search_symbol_combo['values']=search_values_list
+            self.search_symbol_combo.event_generate('<Down>')
 
         except Exception as e:
             msgbx.showerror("Search Symbol Error", str(e))
             self.focus_force()
             return
+    def commandEnterKey(self, event):
+        self.commandSearchSymbol()
